f043cc33c26244c6cbeeb1217faf456fa6ae1593
wwartick <willwartick@gmail.com>
2022-11-27 03:17:39 -0800

    login/logout/profile routes and queries

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -14,8 +18,20 @@ const Login = (props) => {
   };
 
   // submit form
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
+  // submit form
+const handleFormSubmit = async event => {
+  event.preventDefault();
+
+  try {
+    const { data } = await login({
+      variables: { ...formState }
+    });
+
+    Auth.login(data.login.token);
+  } catch (e) {
+    console.error(e);
+  }
+
 
     // clear form values
     setFormState({
@@ -52,6 +68,7 @@ const Login = (props) => {
               <button className='btn d-block w-100' type='submit'>
                 Submit
               </button>
+              {error && <div>Login failed</div>}
             </form>
           </div>
         </div>
